547db0282a53161f24c281511bd9ceee55d2ebb4
SleeperShark <40243105s@gmail.com>
2022-09-01 17:35:34 +0800

    article checkpoint

---
+++ database/migrations/2022_08_31_141949_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            
+            $table->uuid('author_id')->nullable();
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('set null');
+
+            $table->string('title');
+            $table->json('context');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
