97c0524f3775a54e3262b26d3b92a101549746d6
leadbest <leadbest@leadbestdeMBP.localdomain>
2022-09-06 12:10:21 +0800

    Add test.json in database file remove binding in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(UserService::class, function($app){
-            return new UserService();
-        });
+
     }
 
     /**
