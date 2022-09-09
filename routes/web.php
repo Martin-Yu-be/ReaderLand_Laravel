4d1b32b415e9202217e121dced61fae820c08903
leadbest <leadbest@leadbestdeMacBook-Pro.local>
2022-09-09 11:53:12 +0800

    REF: modify /doc to /api/doc

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/doc', function(){
-    // require($_SERVER['DOCUMENT_ROOT']."/../vendor/autoload.php");
-
-
-    $openapi = \OpenApi\Generator::scan([
-        $_SERVER['DOCUMENT_ROOT'].'/../app/Http/Requests/AuthUserRequest.php',
-        $_SERVER['DOCUMENT_ROOT'].'/../app/Http/Requests/StoreUserRequest.php',
-        $_SERVER['DOCUMENT_ROOT'].'/../app/Http/Resources/UserResource.php',
-        $_SERVER['DOCUMENT_ROOT'].'/../app/Http/Resources/ArticleListResource.php',
-        $_SERVER['DOCUMENT_ROOT'].'/../app/Http/Controllers/Api/ArticleController.php',
-        $_SERVER['DOCUMENT_ROOT'].'/../app/Http/Controllers/Api/AuthController.php',
-    ]);
-
-    header('Content-Type: application/json');
-    echo $openapi->toJson();
-});
