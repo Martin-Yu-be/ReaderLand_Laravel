6db0351430819e495545a42a724cd179f1a4e4ed
leadbest <leadbest@leadbestdeMBP.localdomain>
2022-09-12 12:28:44 +0800

    Isolate ArticleSeeder

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Article;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ArticleSeeder extends Seeder
+{
+    public function run()
+    {
+        $json = file_get_contents(__DIR__.'/testcase.json');
+        ['articles' => $articles] = json_decode($json, true);
+
+        //TODO: Get AuthorName => id mapping
+        $userName_id = [];
+        $users = User::select(['id', 'name'])->orderBy('id')->limit(10)->get();
+        foreach($users as $user){
+            $userName_id[$user->name] = $user->id;
+        }
+
+        //TODO: Processing Articles
+        $title_categories = [];
+        for($i = 0; $i < count($articles); $i++){
+            $articles[$i]['head'] = '0';
+
+            // Processing Context
+            $contextArr = explode('||', $articles[$i]['context']);
+            $contextJson = array();
+            foreach($contextArr as $key => $content){
+                $next = $key == count($contextArr) - 1 ? null: (string) ($key+1);
+                $contextJson[(string) $key] = [
+                    'content' => $content,
+                    'type' => 'string',
+                    'next' => $next
+                ];
+            }
+            $articles[$i]['context'] = json_encode($contextJson);
+
+            // Setting userId
+            $articles[$i]['user_id'] =  $userName_id[$articles[$i]['author']];
+            unset($articles[$i]['author']);
+            
+            // Setting count
+            $readCounts = random_int(0, 100);
+            $articles[$i]['read_counts'] = $readCounts;
+            $articles[$i]['like_counts'] = random_int(0, $readCounts);
+            $articles[$i]['comment_counts'] = random_int(0, $readCounts);
+
+            // Setting Category
+            $title_categories[$articles[$i]['title']] = $articles[$i]['category'];
+            unset($articles[$i]['category']);
+        }
+
+        // TODO: Insert Article
+        shuffle($articles);
+        $days = 10;
+        $daysInSeconds = $days * 60 * 60 * 24;
+        $fakeTime = time() - $daysInSeconds;
+        $timeInterval = (int) $daysInSeconds / count($articles);
+
+        foreach($articles as $article){
+            Article::create([
+                ...$article,
+                'created_at' => $fakeTime,
+                'updated_at' => $fakeTime
+            ]);
+            $fakeTime += $timeInterval;
+        }
+
+        //TODO: Fetch category_id mapping
+        $category_id = [];
+        $categories = Category::all();
+        for($i = 0; $i < count($categories); $i++){
+            ['id' => $id, 'category' => $category] = $categories[$i];
+            $category_id[$category] = $id;
+        }
+        
+
+        // TODO: Insert article_categories
+        $articles = Article::all(['id', 'title']);
+        $articles->each(function($article) use($title_categories, $category_id) {
+            ['title' => $title] = $article;
+            $categoryIdArr = array_map(function($catElem) use($category_id){
+                return $category_id[$catElem];
+            }, $title_categories[$title]);
+
+            $article->categories()->attach($categoryIdArr);
+        });    
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
-use App\Models\Article;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -21,90 +18,7 @@ class DatabaseSeeder extends Seeder
             \Database\Seeders\CategorySeeder::class,
             \Database\Seeders\AuthorSeeder::class,
             \Database\Seeders\UserSeeder::class,
+            \Database\Seeders\ArticleSeeder::class,
         ]);
-
-
-        //TODO: Decode json data
-        $json = file_get_contents(__DIR__.'/testcase.json');
-        ['articles' => $articles] = json_decode($json, true);
-        
-        $users = User::all();
-        foreach($users as $user){
-            $userName_id[$user->name] = $user->id;
-        }
-
-        $title_categories = array();
-        // TODO: Process Article
-        for($i = 0; $i < count($articles); $i++){
-            $articles[$i]['head'] = '0';
-            // Processing Context
-            $contextArr = explode('||', $articles[$i]['context']);
-            $contextJson = array();
-            foreach($contextArr as $key => $content){
-                $next = $key == count($contextArr) - 1 ? null: (string) ($key+1);
-                $contextJson[(string) $key] = [
-                    'content' => $content,
-                    'type' => 'stirng',
-                    'next' => $next
-                ];
-            }
-            $articles[$i]['context'] = json_encode($contextJson);
-
-            // Setting userId
-            $articles[$i]['user_id'] =  $userName_id[$articles[$i]['author']];
-            unset($articles[$i]['author']);
-            
-            $readCounts = random_int(0, 100);
-            $articles[$i]['read_counts'] = $readCounts;
-            $articles[$i]['like_counts'] = random_int(0, $readCounts);
-            $articles[$i]['comment_counts'] = random_int(0, $readCounts);
-
-            $title_categories[$articles[$i]['title']] = $articles[$i]['category'];
-            unset($articles[$i]['category']);
-
-        }
-        
-
-        // TODO: Insert Article
-        shuffle($articles);
-        $days = 10;
-        $daysInSeconds = $days * 60 * 60 * 24;
-        $fakeTime = time() - $daysInSeconds;
-        $timeInterval = (int) $daysInSeconds / count($articles);
-
-        foreach($articles as $article){
-            Article::create([
-                ...$article,
-                'created_at' => $fakeTime,
-                'updated_at' => $fakeTime
-            ]);
-            $fakeTime += $timeInterval;
-        }
-
-        // TODO: Organize categories into category => id array
-        $category_id = array();
-        $categories = Category::all();
-
-        foreach($categories as ['category' => $cat, 'id' => $id]){
-            $category_id[$cat] = $id;
-        }
-
-        // TODO: Turn cat string into cat id in $title_categories
-        foreach($title_categories as $key => $cat){
-            $title_categories[$key] = array_map(fn($category) => $category_id[$category], $cat);
-        }
-        // print_r($title_categories);
-
-        // TODO: Insert article_categories
-        $articles = Article::all(['id', 'title']);
-        $articles->each(function($article) use($title_categories) {
-            ['title' => $title, 'id' => $id] = $article;
-            $article->categories()->attach($title_categories[$title]);
-        });
-        
-        // \App\Models\User::factory(5)->create();
-
-
-
     }
 }
