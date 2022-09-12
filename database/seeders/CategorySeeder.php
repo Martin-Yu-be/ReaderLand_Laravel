732dbb6b7c2c2050bacc56e936417d5c6bf4a4ab
leadbest <leadbest@leadbestdeMBP.localdomain>
2022-09-12 10:41:25 +0800

    Isolate CategorySeeder class

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    public function run()
+    {
+        $categories = [
+            '政治與評論',
+            '國際時事',
+            '電影戲劇',
+            '投資理財',
+            '職場產業',
+            '閱讀書評',
+            '創作',
+            'ACG',
+            '文化生活',
+            '旅行美食',
+            '音樂藝文',
+            '健康與情感',
+            '寵物',
+            '個人成長',
+            '親子與教育',
+            '運動',
+            '科學',
+            '心理',
+        ];
+
+        foreach($categories as $category)
+        {
+            \App\Models\Category::create([
+                'category' => $category,
+            ]);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,32 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-                // TODO: Generate Categories
-                $categories = [
-                    '政治與評論',
-                    '國際時事',
-                    '電影戲劇',
-                    '投資理財',
-                    '職場產業',
-                    '閱讀書評',
-                    '創作',
-                    'ACG',
-                    '文化生活',
-                    '旅行美食',
-                    '音樂藝文',
-                    '健康與情感',
-                    '寵物',
-                    '個人成長',
-                    '親子與教育',
-                    '運動',
-                    '科學',
-                    '心理',
-                ];
-                foreach($categories as $category){
-                    \App\Models\Category::create([
-                        'category' => $category
-                    ]);
-                }
+        $this->call([
+            \Database\Seeders\CategorySeeder::class,
+        ]);
 
 
         //TODO: Decode json data
